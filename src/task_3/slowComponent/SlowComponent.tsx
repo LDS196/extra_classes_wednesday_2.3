1ac91a4db0e20c002ea5506731372e943e351dc2
LDS196 <lihachevd87@gmail.com>
2023-02-21 23:19:37 +0400

    Initial commit

---
+++ src/task_3/slowComponent/SlowComponent.tsx
@@ -0,0 +1,16 @@
+import React, {useMemo} from "react";
+
+export const SlowComponent = () => {
+  console.log('SlowComponent re-render...');
+
+  // let now = useMemo(()=>{
+  //    return performance.now()
+  // },[]);
+
+  let now =performance.now()
+  while (performance.now() - now < 1000) {
+    // Artificial delay -- do nothing for 100ms
+  }
+
+  return <p>I am a very slow component tree.</p>;
+};
